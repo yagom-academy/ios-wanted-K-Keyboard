fb429a461a206148c13fa211e0edb98db18d717a
권준상 <wnstkdsun97@naver.com>
2022-09-29 15:15:25 +0900

    feat : Keyboard Extension 연결

--- K-Keyboard/Source/Feature/Keyboard/Keyboard.swift
+++ K-Keyboard/Source/Feature/Keyboard/Keyboard.swift
@@ -5,4 +5,28 @@
 //  Created by KangMingyo on 2022/09/29.
 //
 
-import Foundation
+import UIKit
+
+class KeyboardTestViewController: UIViewController {
+    let textField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .secondarySystemBackground
+        textField.clearButtonMode = .never
+        textField.autocapitalizationType = .none
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(textField)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        textField.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20).isActive = true
+        textField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20).isActive = true
+        textField.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+}
