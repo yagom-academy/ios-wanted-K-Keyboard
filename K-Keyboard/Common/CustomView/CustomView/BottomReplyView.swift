11f75bf2d1064933364cc15693d5795e765c4e11
bang-71 <es.bang71@icloud.com>
2022-09-30 17:01:10 +0900

    feat: Add BottomReplyView

---
+++ K-Keyboard/Common/CustomView/CustomView/BottomReplyView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomReplyView.swift
+//  K-Keyboard
+//
+//  Created by 신병기 on 2022/09/30.
+//
+
+import UIKit
+
+class BottomReplyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
