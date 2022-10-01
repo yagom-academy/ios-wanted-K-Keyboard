3faeb3ac8f1aef3e11123378e4eaeac1b587f633
julia0926 <kj980926@naver.com>
2022-10-02 00:15:20 +0900

    feature : tagList 스택뷰로 구현
    컬렉션뷰로 변경 예정

--- K-Keyboard/MainViewController.swift
+++ K-Keyboard/MainViewController.swift
@@ -7,18 +7,26 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
 
-    @IBOutlet weak var tagListStackView: UIStackView!
-    @IBOutlet weak var keywordColletionView: UICollectionView!
-    @IBOutlet weak var themeStackView: UIStackView!
+    @IBOutlet private weak var tagListStackView: UIStackView!
+    @IBOutlet private weak var keywordColletionView: UICollectionView!
+    @IBOutlet private weak var themeStackView: UIStackView!
     
+    private var tagList = ["이벤트", "캐릭터", "새", "동물", "앙증맞은" ,"동글동글"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setTagList()
     }
 
-
+    private func setTagList() {
+        tagList.enumerated().forEach {
+            let tagView = TagView()
+            tagView.heightAnchor.constraint(equalToConstant: 20).isActive = true
+            tagView.configure(title: $1)
+            tagListStackView.addArrangedSubview(tagView)
+        }
+    }
 }
 
---
+++ K-Keyboard/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  K-Keyboard
+//
+//  Created by 김지인 on 2022/10/01.
+//
+
+import Foundation
