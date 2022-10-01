6387ad263d784878692b8c7209e3978587c7bef0
julia0926 <kj980926@naver.com>
2022-10-01 13:59:02 +0900

    style : outlet 연결, mainVC renaming

--- K-Keyboard/MainViewController.swift
+++ K-Keyboard/MainViewController.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet weak var tagListStackView: UIStackView!
+    @IBOutlet weak var keywordColletionView: UICollectionView!
+    @IBOutlet weak var themeStackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
