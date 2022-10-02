dd8abd537d91359c2ac91cc53280679572932de8
권준상 <wnstkdsun97@naver.com>
2022-10-02 18:17:38 +0900

    feat : 모음만 있을때 입력, 자음 + 모음 입력시 알고리즘 구현

--- K-Keyboard/Source/Feature/Keyboard/KoreanAutomata.swift
+++ K-Keyboard/Source/Feature/Keyboard/KoreanAutomata.swift
@@ -20,7 +20,9 @@ class KoreanAutomata {
     
     let vowels: [String] = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ",
                                "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
-    let doubleVowels: [String] = []
+    let doubleVowels: [String : [String]] = ["ㅗ": ["ㅏ", "ㅐ", "ㅣ", "ㅘ", "ㅙ", "ㅚ"],
+                                             "ㅜ": ["ㅓ", "ㅔ", "ㅣ", "ㅝ", "ㅞ", "ㅟ"],
+                                             "ㅡ": ["ㅣ", "ㅢ"]]
     let finalConsonants: [String] = [" ", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ","ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
     var lastWord = ""
     var totalWords: [String] = []
@@ -43,7 +45,7 @@ class KoreanAutomata {
             }
         case .firstConsonantOnly:
             if keyType == .vowel {
-                let value = hangle(chosung: lastWord, jungsung: text, jongsung: nil)
+                let value = combinationToComplete(chosung: lastWord, jungsung: text, jongsung: nil)
                 lastWord = value ?? ""
                 totalWords.append(text)
                 return (.consonantPlusVowel, lastWord)
@@ -59,19 +61,103 @@ class KoreanAutomata {
                 }
             }
         case .vowelOnly:
-            return (.start, text)
+            if keyType == .vowel {
+                if doubleVowels.keys.contains(lastWord) {
+                    if let isContain = doubleVowels[lastWord]?.contains(text) {
+                        if isContain {
+                            if lastWord == "ㅡ" {
+                                lastWord = (doubleVowels[lastWord]?.last)!
+                            } else {
+                                let index = doubleVowels[lastWord]?.firstIndex(of: text)
+                                lastWord = (doubleVowels[lastWord]?[index! + 3])!
+                            }
+                            totalWords.append(text)
+                            return (.vowelOnly, lastWord)
+                        } else {
+                            lastWord = text
+                            totalWords.append(text)
+                            return (.vowelOnly, lastWord)
+                        }
+                    } else {
+                        print("dictionary error")
+                    }
+                } else {
+                    lastWord = text
+                    totalWords.append(text)
+                    return (.vowelOnly, lastWord)
+                }
+            } else {
+                lastWord = text
+                totalWords.append(text)
+                return (.firstConsonantOnly, lastWord)
+            }
         case .consonantPlusVowel:
-            return (.start, text)
+            if keyType == .consonant {
+                var (choUni, jungUni, jongUni) = completeToCombination(word: lastWord)
+                
+                for i in 0 ..< finalConsonants.count {
+                    if finalConsonants[i] == text { jongUni = i }
+                }
+                
+                if jongUni == 0 {
+                    lastWord = text
+                    totalWords.append(text)
+                    return (.firstConsonantOnly, lastWord)
+                } else {
+                    let uniValue:Int = (choUni * 21 * 28) + (jungUni * 28) + (jongUni) + 0xAC00;
+                    if let uni = Unicode.Scalar(uniValue) {
+                        lastWord = String(Character(uni))
+                        return (.finalConsonant, lastWord)
+                    }
+                }
+            } else {
+                var (choUni, jungUni, _) = completeToCombination(word: lastWord)
+                
+                var lastVowel = totalWords.last!
+                if doubleVowels.keys.contains(lastVowel) {
+                    if let isContain = doubleVowels[lastVowel]?.contains(text) {
+                        if isContain {
+                            if lastVowel == "ㅡ" {
+                                lastVowel = (doubleVowels[lastVowel]?.last)!
+                            } else {
+                                let index = doubleVowels[lastVowel]?.firstIndex(of: text)
+                                lastVowel = (doubleVowels[lastVowel]?[index! + 3])!
+                            }
+                            totalWords.append(text)
+
+                            for i in 0 ..< vowels.count {
+                                if vowels[i] == lastVowel { jungUni = i }
+                            }
+                            
+                            let uniValue:Int = (choUni * 21 * 28) + (jungUni * 28) + 0xAC00;
+                            if let uni = Unicode.Scalar(uniValue) {
+                                lastWord = String(Character(uni))
+                                return (.consonantPlusVowel, lastWord)
+                            }
+                        } else {
+                            lastWord = text
+                            totalWords.append(text)
+                            return (.vowelOnly, lastWord)
+                        }
+                } else {
+                    lastWord = text
+                    totalWords.append(text)
+                    return (.vowelOnly, lastWord)
+                }
+            } else {
+                print("unicode transformation error")
+            }
+        }
         case .finalConsonant:
             return (.start, text)
         }
         
         
         
-        
+        return (.start, text)
     }
     
-    func hangle(chosung: String, jungsung: String, jongsung:String?) -> String? {
+    func combinationToComplete(chosung: String, jungsung: String, jongsung:String?) -> String? {
         var chosungIndex = 0
         var jungsungIndex = 0
         var jongsungIndex = 0
@@ -94,6 +180,14 @@ class KoreanAutomata {
         }
         
         return nil
+    }
+    
+    func completeToCombination(word: String) -> (Int, Int, Int) {
+        let completeUniValue = word.utf16.map { Int($0) }.reduce(0, +)
+        let jongsungUniValue: Int = (completeUniValue - 0xAC00) % 28
+        let jungsungUniValue: Int = (completeUniValue - 0xAC00) % (28 * 21) / 28
+        let chosungUniValue: Int =  (completeUniValue - 0xAC00) / (28 * 21)
+        return (chosungUniValue, jungsungUniValue, jongsungUniValue)
     }
     
 }
