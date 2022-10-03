2769dba3140859e12fc46a78c2e480fd8fe926e8
JunYoungJee <rainmaker8492@gmail.com>
2022-10-04 00:17:04 +0900

    keyboardVC 경량화 중
    이걸 이제 mvvm으로 어떻게 변환해야 하나 고려를 해야...

--- CustomKeyboard/Models/KeyboardModel.swift
+++ CustomKeyboard/Models/KeyboardModel.swift
@@ -5,7 +5,10 @@
 //  Created by pablo.jee on 2022/10/03.
 //
 
+// TODO: Model에 UIKit이 임포트 되어야 하는 상황 자체는 좋지 않음
+//다른 구조, 패턴으로 수정 필요
 import Foundation
+import UIKit
 
 class KeyboardModel {
     //input
@@ -13,10 +16,10 @@ class KeyboardModel {
     //output
     
     //properties
+    var textDocumentProxy: UITextDocumentProxy
     
-    
-    init() {
-        
+    init(textDocumentProxy: UITextDocumentProxy) {
+        self.textDocumentProxy = textDocumentProxy
     }
     
     func decompose(char: Character) -> (Character, Character, Character) {
@@ -47,4 +50,102 @@ class KeyboardModel {
         return Hangle.exceptionForJongSung(jong: jong, input: input)
     }
     
+}
+
+extension KeyboardModel : KeyboardViewDelegate{
+    
+    func insertSpace(){
+        textDocumentProxy.insertText(" ")
+    }
+    
+    func insertCharacter(_ newCharacter: String) {
+        //characterBeforeCursor호출하여 전 글자를 가져온다
+        guard let last = characterBeforeCursor(), last != " " else {
+            //전 글자가 없다면 그냥 입력
+            textDocumentProxy.insertText(newCharacter)
+            return
+        }
+        //전 글자가 있다면 합성할 수 있는지 없는지 검사(같은 프로토콜인지 아닌지 검사)
+        let char = Character(newCharacter)
+        let (cho,jung,jong) = decompose(char: last)
+        //newCharacter가 모음인지 자음인지 검사
+        if isVowel(char){ //newCharacter가 모음
+            if jong != " "{//1.종성이 있을 때
+                
+                if isDoubleFinalConsonant(jong){ // 1.1 종성이 겹받침일 때
+                    let oldJong = decomposeDoubleFinalConsonant(jong).0
+                    let newJong = decomposeDoubleFinalConsonant(jong).1
+                    textDocumentProxy.deleteBackward()
+                    textDocumentProxy.insertText(String(compose(c1: cho, c2: jung, c3: oldJong)))
+                    textDocumentProxy.insertText(String(compose(c1: newJong, c2: char, c3: " ")))
+                }else{ //1.2 종성이 겹받침이 아닐 때
+                    textDocumentProxy.deleteBackward()
+                    textDocumentProxy.insertText(String(compose(c1: cho, c2: jung, c3: " ")))
+                    textDocumentProxy.insertText(String(compose(c1: jong, c2: char, c3: " ")))
+                }
+            }else if jung != " "{//2.종성은 없고 중성이 있을 때
+                if let newJung = exceptionForJungSung(jung: jung, input: char){//2.1 중성이 이중모음되는 것이 가능할 때
+                    textDocumentProxy.deleteBackward()
+                    textDocumentProxy.insertText(String(compose(c1: cho, c2: newJung, c3: " ")))
+                }else{//2.2 중성이 이중모음되는 것이 불가능할 때
+                    textDocumentProxy.insertText(String(compose(c1: char, c2: " ", c3: " ")))
+                }
+            }else{//3.초성만 있을 때
+                textDocumentProxy.deleteBackward()
+                textDocumentProxy.insertText(String(compose(c1: cho, c2: char, c3: " ")))
+            }
+        }else{ //newCharacter가 자음
+            if jong != " "{//1.종성이 있을 때
+                if isDoubleFinalConsonant(jong){ // 1.1 종성이 겹받침일 때
+                    textDocumentProxy.insertText(String(compose(c1: char, c2: " ", c3: " ")))
+                }else{ //1.2 종성이 겹받침이 아닐 때
+                    if let doubleConsonant = exceptionForJongSung(jong: jong, input: char){//1.2.1 새로 입력되는 글자가 겹받침이 가능할 때
+                        textDocumentProxy.deleteBackward()
+                        textDocumentProxy.insertText(String(compose(c1: cho, c2: jung, c3: doubleConsonant)))
+                    }else{//1.2.2 새로 입력되는 글자가 겹받침이 불가능할 때
+                        textDocumentProxy.insertText(String(compose(c1: char, c2: " ", c3: " ")))
+                    }
+                }
+            }else if jung != " "{//2.종성은 없고 중성이 있을 때
+                    textDocumentProxy.deleteBackward()
+                    textDocumentProxy.insertText(String(compose(c1: cho, c2: jung, c3: char)))
+            }else{//3.초성만 있을 때
+                textDocumentProxy.insertText(String(compose(c1: char, c2:" ",c3:" ")))
+            }
+        }
+    }
+    
+    func deleteCharacterBeforeCursor() {
+        //characterBeforeCursor호출하여 지울 글자를 가져온다
+        guard let last = characterBeforeCursor(), last != " " else {
+            textDocumentProxy.deleteBackward()
+            return
+        }
+        //가져온 글자를 분석한다
+        //만약 공백이면 초성,중성,종성 모두 공백으로 반환
+        let (cho,jung,jong) = decompose(char: last)
+        //1.종성까지 모두 다 있는 상황
+        if jong != " "{
+            if isDoubleFinalConsonant(jong){//1-1.겹받침
+                let newJong = decomposeDoubleFinalConsonant(jong).0
+                textDocumentProxy.deleteBackward()
+                textDocumentProxy.insertText(String(compose(c1: cho, c2: jung, c3: newJong)))
+            }else{//1-2.일반받침
+                textDocumentProxy.deleteBackward()
+                textDocumentProxy.insertText(String(compose(c1: cho, c2: jung, c3: " ")))
+            }
+        }else if jung != " "{//2.중성까지 있는 상황
+            textDocumentProxy.deleteBackward()
+            textDocumentProxy.insertText(String(compose(c1: cho, c2: " ", c3: " ")))
+        }else{//3.초성만 있는 상황 && 공백인 상황
+            textDocumentProxy.deleteBackward()
+        }
+    }
+    
+    func characterBeforeCursor() -> Character? {
+        guard let character = textDocumentProxy.documentContextBeforeInput?.last else {
+          return nil
+        }
+        return character
+    }
 }
--- CustomKeyboard/Views/KeyboardViewController.swift
+++ CustomKeyboard/Views/KeyboardViewController.swift
@@ -22,7 +22,7 @@ class KeyboardViewController: UIInputViewController {
     
     @IBOutlet var nextKeyboardButton: UIButton!
     
-    var model: KeyboardModel = KeyboardModel()
+    lazy var model: KeyboardModel = KeyboardModel(textDocumentProxy: self.textDocumentProxy)
     
     lazy var backView : KeyboardView = {
         let view = KeyboardView(frame: .zero)
@@ -36,19 +36,9 @@ class KeyboardViewController: UIInputViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubviews(backView)
-        NSLayoutConstraint.activate([
-            backView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            backView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-            backView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            backView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
-        ])
-        backView.firstFloor.setNextKeyboardVisible(needsInputModeSwitchKey)
-        backView.firstFloor.btnNext.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-        backView.firstFloor.delegate = self
-        backView.secondFloor.delegate = self
-        backView.thridFloor.delegate = self
-        backView.forthFloor.delegate = self
+        initViewHierarchy()
+        configureView()
+        bind()
     }
 
     
@@ -64,9 +54,10 @@ class KeyboardViewController: UIInputViewController {
     
     override func textDidChange(_ textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
+        
+        // TODO: 얘는 어떻게 쓰는 건지 확인을 해봐야...
         var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+        if model.textDocumentProxy.keyboardAppearance == UIKeyboardAppearance.dark {
             textColor = UIColor.white
         } else {
             textColor = UIColor.black
@@ -79,7 +70,14 @@ class KeyboardViewController: UIInputViewController {
 //LoadView를 넣으니 앱이 죽음...익스텐션은 다른가 좀 확인 필요...
 extension KeyboardViewController: Presentable {
     func initViewHierarchy() {
-        
+        self.view.addSubviews(backView)
+        NSLayoutConstraint.activate([
+            backView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            backView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            backView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            backView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+        backView.firstFloor.setNextKeyboardVisible(needsInputModeSwitchKey)
     }
     
     func configureView() {
@@ -87,106 +85,11 @@ extension KeyboardViewController: Presentable {
     }
     
     func bind() {
-        
+        backView.firstFloor.btnNext.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        backView.firstFloor.delegate = self.model
+        backView.secondFloor.delegate = self.model
+        backView.thridFloor.delegate = self.model
+        backView.forthFloor.delegate = self.model
     }
-    
-    
 }
 
-extension KeyboardViewController : KeyboardViewDelegate{
-    
-    func insertSpace(){
-        textDocumentProxy.insertText(" ")
-    }
-    
-    func insertCharacter(_ newCharacter: String) {
-        //characterBeforeCursor호출하여 전 글자를 가져온다
-        guard let last = characterBeforeCursor(), last != " " else {
-            //전 글자가 없다면 그냥 입력
-            textDocumentProxy.insertText(newCharacter)
-            return
-        }
-        //전 글자가 있다면 합성할 수 있는지 없는지 검사(같은 프로토콜인지 아닌지 검사)
-        let char = Character(newCharacter)
-        let (cho,jung,jong) = model.decompose(char: last)
-        //newCharacter가 모음인지 자음인지 검사
-        if model.isVowel(char){ //newCharacter가 모음
-            if jong != " "{//1.종성이 있을 때
-                
-                if model.isDoubleFinalConsonant(jong){ // 1.1 종성이 겹받침일 때
-                    let oldJong = model.decomposeDoubleFinalConsonant(jong).0
-                    let newJong = model.decomposeDoubleFinalConsonant(jong).1
-                    textDocumentProxy.deleteBackward()
-                    textDocumentProxy.insertText(String(model.compose(c1: cho, c2: jung, c3: oldJong)))
-                    textDocumentProxy.insertText(String(model.compose(c1: newJong, c2: char, c3: " ")))
-                }else{ //1.2 종성이 겹받침이 아닐 때
-                    textDocumentProxy.deleteBackward()
-                    textDocumentProxy.insertText(String(model.compose(c1: cho, c2: jung, c3: " ")))
-                    textDocumentProxy.insertText(String(model.compose(c1: jong, c2: char, c3: " ")))
-                }
-            }else if jung != " "{//2.종성은 없고 중성이 있을 때
-                if let newJung = model.exceptionForJungSung(jung: jung, input: char){//2.1 중성이 이중모음되는 것이 가능할 때
-                    textDocumentProxy.deleteBackward()
-                    textDocumentProxy.insertText(String(model.compose(c1: cho, c2: newJung, c3: " ")))
-                }else{//2.2 중성이 이중모음되는 것이 불가능할 때
-                    textDocumentProxy.insertText(String(model.compose(c1: char, c2: " ", c3: " ")))
-                }
-            }else{//3.초성만 있을 때
-                textDocumentProxy.deleteBackward()
-                textDocumentProxy.insertText(String(model.compose(c1: cho, c2: char, c3: " ")))
-            }
-        }else{ //newCharacter가 자음
-            if jong != " "{//1.종성이 있을 때
-                if model.isDoubleFinalConsonant(jong){ // 1.1 종성이 겹받침일 때
-                    textDocumentProxy.insertText(String(model.compose(c1: char, c2: " ", c3: " ")))
-                }else{ //1.2 종성이 겹받침이 아닐 때
-                    if let doubleConsonant = model.exceptionForJongSung(jong: jong, input: char){//1.2.1 새로 입력되는 글자가 겹받침이 가능할 때
-                        textDocumentProxy.deleteBackward()
-                        textDocumentProxy.insertText(String(model.compose(c1: cho, c2: jung, c3: doubleConsonant)))
-                    }else{//1.2.2 새로 입력되는 글자가 겹받침이 불가능할 때
-                        textDocumentProxy.insertText(String(model.compose(c1: char, c2: " ", c3: " ")))
-                    }
-                }
-            }else if jung != " "{//2.종성은 없고 중성이 있을 때
-                    textDocumentProxy.deleteBackward()
-                    textDocumentProxy.insertText(String(model.compose(c1: cho, c2: jung, c3: char)))
-            }else{//3.초성만 있을 때
-                textDocumentProxy.insertText(String(model.compose(c1: char, c2:" ",c3:" ")))
-            }
-        }
-    }
-    
-    func deleteCharacterBeforeCursor() {
-        //characterBeforeCursor호출하여 지울 글자를 가져온다
-        guard let last = characterBeforeCursor(), last != " " else {
-            textDocumentProxy.deleteBackward()
-            return
-        }
-        //가져온 글자를 분석한다
-        //만약 공백이면 초성,중성,종성 모두 공백으로 반환
-        let (cho,jung,jong) = model.decompose(char: last)
-        //1.종성까지 모두 다 있는 상황
-        if jong != " "{
-            if model.isDoubleFinalConsonant(jong){//1-1.겹받침
-                let newJong = model.decomposeDoubleFinalConsonant(jong).0
-                textDocumentProxy.deleteBackward()
-                textDocumentProxy.insertText(String(model.compose(c1: cho, c2: jung, c3: newJong)))
-            }else{//1-2.일반받침
-                textDocumentProxy.deleteBackward()
-                textDocumentProxy.insertText(String(model.compose(c1: cho, c2: jung, c3: " ")))
-            }
-        }else if jung != " "{//2.중성까지 있는 상황
-            textDocumentProxy.deleteBackward()
-            textDocumentProxy.insertText(String(model.compose(c1: cho, c2: " ", c3: " ")))
-        }else{//3.초성만 있는 상황 && 공백인 상황
-            textDocumentProxy.deleteBackward()
-        }
-    }
-    
-    func characterBeforeCursor() -> Character? {
-        guard let character = textDocumentProxy.documentContextBeforeInput?.last else {
-          return nil
-        }
-        return character
-    }
-}
