f65dcefb9e2ca7184cdb479f6ee44fbacd90492f
SChan-Kim <instufld@gmail.com>
2022-10-07 15:44:25 +0900

    refactor: korKeyboardView 에서 UIButton 생성 시기 수정

--- K-Keyboard/View/Keyboard/KorKeyboardView.swift
+++ K-Keyboard/View/Keyboard/KorKeyboardView.swift
@@ -20,62 +20,27 @@ class KorKeyboardView: UIView {
     var letter: String { return KoreanData.letter(stack, state) }
     var isShiftPressed: Bool = false
     
+    enum ButtonType {
+        case image
+        case text
+    }
+    
+    lazy var shiftButton = UIButton()
+    lazy var deleteButton = UIButton()
+    lazy var spaceBar = UIButton()
+    lazy var returnButton = UIButton()
+    
+    // 키보드 각 행을 나타내는 stackView
     lazy var horizontal1 = UIStackView()
     lazy var horizontal1Shift = UIStackView()
     lazy var horizontal2 = UIStackView()
     lazy var horizontal3 = UIStackView()
-
-    let shift: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(systemName: "shift"), for: .normal)
-        button.layer.cornerRadius = 5
-        button.backgroundColor = .white
-        button.tintColor = .black
-        button.contentMode = .scaleAspectFit
-        button.addTarget(self, action: #selector(shiftPressed), for: .touchUpInside)
-        
-        return button
-    }()
     
-    let delete: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(systemName: "delete.backward"), for: .normal)
-        button.layer.cornerRadius = 5
-        button.backgroundColor = .white
-        button.tintColor = .black
-        button.contentMode = .scaleAspectFit
-        button.addTarget(self, action: #selector(deletePressed), for: .touchUpInside)
-
-        return button
-    }()
-
-    let spaceBar: UIButton = {
-        let button = UIButton()
-        button.layer.cornerRadius = 5
-        button.setTitle("space", for: .normal)
-        button.backgroundColor = .white
-        button.setTitleColor(.black, for: .normal)
-        button.addTarget(self, action: #selector(spacePressed), for: .touchUpInside)
-        return button
-    }()
-
-    let returnButton: UIButton = {
-        let button = UIButton()
-        button.layer.cornerRadius = 5
-        button.setTitle("return", for: .normal)
-        button.backgroundColor = .white
-        button.setTitleColor(.black, for: .normal)
-        return button
-    }()
-
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-        horizontal1 = makeHorizontal1()
-        horizontal1Shift = makeHorizontal1Shift()
-        horizontal2 = makeHorizontal2()
-        horizontal3 = makeHorizontal3(shift: self.shift, delete: self.delete)
         
+        makeButtons()
+        makeStackViews()
         addViews()
         setConstraints()
     }
@@ -86,6 +51,76 @@ class KorKeyboardView: UIView {
 }
 
 extension KorKeyboardView {
+    func makeButtons() {
+        shiftButton = makeButton(type: .image, imageStr: "shift")
+        shiftButton.addTarget(self, action: #selector(shiftPressed), for: .touchUpInside)
+      
+        deleteButton = makeButton(type: .image, imageStr: "delete.backward")
+        deleteButton.addTarget(self, action: #selector(deletePressed), for: .touchUpInside)
+        
+        spaceBar = makeButton(type: .text, titleStr: "space")
+        spaceBar.addTarget(self, action: #selector(spacePressed), for: .touchUpInside)
+        
+        returnButton = makeButton(type: .text, titleStr: "return")
+    }
+    
+    func makeStackViews() {
+        horizontal1 = makeHorizontal1()
+        horizontal1Shift = makeHorizontal1Shift()
+        horizontal2 = makeHorizontal2()
+        horizontal3 = makeHorizontal3(shift: self.shiftButton, delete: self.deleteButton)
+    }
+    
+    func addViews() {
+        [horizontal1, horizontal1Shift, horizontal2, horizontal3, spaceBar, returnButton].forEach { self.addSubview($0) }
+    }
+    
+    func setConstraints() {
+        [horizontal1, horizontal1Shift, horizontal2, horizontal3, spaceBar, returnButton].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        
+        NSLayoutConstraint.activate([
+            horizontal1.topAnchor.constraint(equalTo: self.topAnchor),
+            horizontal1.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            horizontal1.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            horizontal1Shift.topAnchor.constraint(equalTo: horizontal1.topAnchor),
+            horizontal1Shift.leadingAnchor.constraint(equalTo: horizontal1.leadingAnchor),
+            horizontal1Shift.trailingAnchor.constraint(equalTo: horizontal1.trailingAnchor),
+            horizontal1Shift.bottomAnchor.constraint(equalTo: horizontal1.bottomAnchor),
+            horizontal2.topAnchor.constraint(equalTo: horizontal1.bottomAnchor, constant: 12),
+            horizontal2.leadingAnchor.constraint(equalTo: horizontal1.leadingAnchor),
+            horizontal2.trailingAnchor.constraint(equalTo: horizontal1.trailingAnchor),
+            horizontal3.topAnchor.constraint(equalTo: horizontal2.bottomAnchor, constant: 12),
+            horizontal3.leadingAnchor.constraint(equalTo: horizontal1.leadingAnchor),
+            horizontal3.trailingAnchor.constraint(equalTo: horizontal1.trailingAnchor),
+            spaceBar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 4),
+            spaceBar.topAnchor.constraint(equalTo: horizontal3.bottomAnchor, constant: 12),
+            spaceBar.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -4),
+            returnButton.leadingAnchor.constraint(equalTo: spaceBar.trailingAnchor, constant: 6),
+            returnButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -4),
+            returnButton.bottomAnchor.constraint(equalTo: spaceBar.bottomAnchor),
+            returnButton.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3)
+        ])
+    }
+}
+
+extension KorKeyboardView {
+    func makeButton(type: ButtonType, imageStr: String = "", titleStr: String = "") -> UIButton {
+        let button = UIButton()
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .white
+        
+        switch type {
+        case .image:
+            button.setBackgroundImage(UIImage(systemName: imageStr), for: .normal)
+            button.tintColor = .black
+            button.contentMode = .scaleAspectFit
+        case .text:
+            button.setTitle(titleStr, for: .normal)
+            button.setTitleColor(.black, for: .normal)
+        }
+        
+        return button
+    }
     
     func makeHorizontal1() -> UIStackView {
         let row1 = "ㅂㅈㄷㄱㅅㅛㅕㅑㅐㅔ".map { String($0) }
@@ -184,39 +219,5 @@ extension KorKeyboardView {
         stackView.setCustomSpacing(16, after: stackView.arrangedSubviews[row3.count])
 
         return stackView
-    }
-
-}
-
-extension KorKeyboardView {
-    func addViews() {
-        [horizontal1, horizontal1Shift, horizontal2, horizontal3, spaceBar, returnButton].forEach { self.addSubview($0) }
-    }
-    
-    func setConstraints() {
-        [horizontal1, horizontal1Shift, horizontal2, horizontal3, spaceBar, returnButton].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
-        
-        NSLayoutConstraint.activate([
-            horizontal1.topAnchor.constraint(equalTo: self.topAnchor),
-            horizontal1.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            horizontal1.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            horizontal1Shift.topAnchor.constraint(equalTo: horizontal1.topAnchor),
-            horizontal1Shift.leadingAnchor.constraint(equalTo: horizontal1.leadingAnchor),
-            horizontal1Shift.trailingAnchor.constraint(equalTo: horizontal1.trailingAnchor),
-            horizontal1Shift.bottomAnchor.constraint(equalTo: horizontal1.bottomAnchor),
-            horizontal2.topAnchor.constraint(equalTo: horizontal1.bottomAnchor, constant: 12),
-            horizontal2.leadingAnchor.constraint(equalTo: horizontal1.leadingAnchor),
-            horizontal2.trailingAnchor.constraint(equalTo: horizontal1.trailingAnchor),
-            horizontal3.topAnchor.constraint(equalTo: horizontal2.bottomAnchor, constant: 12),
-            horizontal3.leadingAnchor.constraint(equalTo: horizontal1.leadingAnchor),
-            horizontal3.trailingAnchor.constraint(equalTo: horizontal1.trailingAnchor),
-            spaceBar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 4),
-            spaceBar.topAnchor.constraint(equalTo: horizontal3.bottomAnchor, constant: 12),
-            spaceBar.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -4),
-            returnButton.leadingAnchor.constraint(equalTo: spaceBar.trailingAnchor, constant: 6),
-            returnButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -4),
-            returnButton.bottomAnchor.constraint(equalTo: spaceBar.bottomAnchor),
-            returnButton.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3)
-        ])
     }
 }
