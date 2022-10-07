95536a23fc67e81e1d2816115006895e3c01af6a
JunYoungJee <rainmaker8492@gmail.com>
2022-10-07 18:38:04 +0900

    팝업뷰 스타일 먹이는 부분 구현 수정

--- K-Keyboard/Views/SecondVCRelated/SecondView.swift
+++ K-Keyboard/Views/SecondVCRelated/SecondView.swift
@@ -14,147 +14,85 @@ protocol SecondViewProtocol{
     func dismissSecondView()
 }
 
-class SecondView : UIView {
+class SecondView : UIView, SecondViewStyling {
     
     var delegate : SecondViewProtocol?
     
     lazy var tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissSecondView))
     
-    let secondView : UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        view.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-        view.layer.cornerRadius = 24
-        view.clipsToBounds = false
-        return view
-    }()
+    let secondView : UIView = UIView()
     
-    lazy var purchaseBtn : UIButton = {
-        let btn = UIButton()
-        btn.setTitle("충전하고 바로 사용하기", for: .normal)
-        btn.layer.backgroundColor = UIColor(red: 1, green: 0.255, blue: 0.49, alpha: 1).cgColor
-        btn.layer.cornerRadius = 24
-        btn.addTarget(self, action: #selector(vertifyAndDismissSecondView), for: .touchUpInside)
-        return btn
-    }()
+    lazy var purchaseBtn : UIButton = UIButton()
     
-    let imageView : UIImageView = {
-        let img = UIImage(named: "Image")
-        let imgView = UIImageView(image: img)
-        imgView.contentMode = .scaleAspectFit
-        return imgView
-    }()
+    let imageView : UIImageView = UIImageView()
     
-    let gemImageView : UIImageView = {
-        let img = UIImage(named: "Gem")
-        let imgView = UIImageView(image: img)
-        imgView.contentMode = .scaleAspectFit
-        return imgView
-    }()
+    let gemImageView : UIImageView = UIImageView()
     
-    let leftButtonImageView : UIImageView = {
-        let img = UIImage(named: "Left")
-        let imgView = UIImageView(image: img)
-        imgView.contentMode = .scaleAspectFit
-        return imgView
-    }()
+    let leftButtonImageView : UIImageView = UIImageView()
     
-    let rightButtonImageImageView : UIImageView = {
-        let img = UIImage(named: "Right")
-        let imgView = UIImageView(image: img)
-        imgView.contentMode = .scaleAspectFit
-        return imgView
-    }()
+    let rightButtonImageImageView : UIImageView = UIImageView()
     
-    let label1 : UILabel = {
-        let lbl = UILabel()
-        lbl.text = "N젬이 부족해요\n빠르게 충전해 보세요!"
-        lbl.textColor = UIColor(red: 0.259, green: 0.267, blue: 0.298, alpha: 1)
-        lbl.font = UIFont(name: "NotoSansCJKKR-Bold", size: 16)
-        lbl.numberOfLines = 0
-        lbl.lineBreakMode = .byWordWrapping
-        var paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineHeightMultiple = 1.01
-        lbl.textAlignment = .center
-        let color = UIColor(red: 0.49, green: 0.788, blue: 0.988, alpha: 1)
-        let fullText = lbl.text ?? ""
-        let range = (fullText as NSString).range(of: "N젬")
-        let attributedString = NSMutableAttributedString(string: fullText)
-        attributedString.addAttribute(.foregroundColor, value: color, range: range)
-        lbl.attributedText = attributedString
-        return lbl
-    }()
+    let label1 : UILabel = UILabel()
     
-    let label2 : UILabel = {
-        let lbl = UILabel()
-        lbl.textColor = UIColor(red: 0.259, green: 0.267, blue: 0.298, alpha: 1)
-        lbl.font = UIFont(name: "NotoSansCJKKR-Bold", size: 14)
-        var paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineHeightMultiple = 1.16
-        lbl.textAlignment = .center
-        lbl.attributedText = NSMutableAttributedString(string: "젬 수량", attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
-        return lbl
-    }()
+    let label2 : UILabel = UILabel()
     
-    let label3 : UILabel = {
-        let lbl = UILabel()
-        lbl.textColor = UIColor(red: 0.259, green: 0.267, blue: 0.298, alpha: 1)
-        lbl.font = UIFont(name: "NotoSansCJKKR-Bold", size: 14)
-        var paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineHeightMultiple = 1.16
-        lbl.textAlignment = .center
-        lbl.attributedText = NSMutableAttributedString(string: "결제 금액", attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
-        return lbl
-    }()
+    let label3 : UILabel = UILabel()
     
-    let label4 : UILabel = {
-        let lbl = UILabel()
-        lbl.textColor = UIColor(red: 1, green: 0.255, blue: 0.49, alpha: 1)
-        lbl.font = UIFont(name: "NotoSansKR-Medium", size: 14)
-        var paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineHeightMultiple = 0.99
-        lbl.textAlignment = .center
-        lbl.attributedText = NSMutableAttributedString(string: "₩ 1,100", attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
-        return lbl
-    }()
+    let label4 : UILabel = UILabel()
     
     
     init() {
         super.init(frame: .zero)
-        addViews()
-        setConstraint()
-        configuration()
-        self.layer.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+        
+        initViewHierarchy()
+        configureView()
+        bind()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func addViews(){
-        self.addSubview(secondView)
-        secondView.translatesAutoresizingMaskIntoConstraints = false
-        secondView.addSubview(purchaseBtn)
-        purchaseBtn.translatesAutoresizingMaskIntoConstraints = false
-        secondView.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        secondView.addSubview(label1)
-        label1.translatesAutoresizingMaskIntoConstraints = false
-        secondView.addSubview(label2)
-        label2.translatesAutoresizingMaskIntoConstraints = false
-        secondView.addSubview(leftButtonImageView)
-        leftButtonImageView.translatesAutoresizingMaskIntoConstraints = false
-        secondView.addSubview(gemImageView)
-        gemImageView.translatesAutoresizingMaskIntoConstraints = false
-        secondView.addSubview(rightButtonImageImageView)
-        rightButtonImageImageView.translatesAutoresizingMaskIntoConstraints = false
-        secondView.addSubview(label3)
-        label3.translatesAutoresizingMaskIntoConstraints = false
-        secondView.addSubview(label4)
-        label4.translatesAutoresizingMaskIntoConstraints = false
+    @objc func vertifyAndDismissSecondView(){
+        delegate?.confirmAndDismissSecondView()
     }
     
-    func setConstraint(){
+    @objc func dismissSecondView(_ sender : UITapGestureRecognizer){
+        //투명한 화면 뿐 아니라 하얀 화면을 클릭해도 함수가 발동되어 제약 사항 추가
+        if sender.view?.hitTest(sender.location(in: self), with: nil) is Self{
+            delegate?.dismissSecondView()
+        }
+    }
+}
+
+extension SecondView: Presentable {
+    func initViewHierarchy() {
+        self.addSubview(secondView)
+        secondView.translatesAutoresizingMaskIntoConstraints = false
+        
+        secondView.addSubview(purchaseBtn)
+        
+        secondView.addSubview(imageView)
+        
+        secondView.addSubview(label1)
+        
+        secondView.addSubview(label2)
+        
+        secondView.addSubview(leftButtonImageView)
+        
+        secondView.addSubview(gemImageView)
+        
+        secondView.addSubview(rightButtonImageImageView)
+        
+        secondView.addSubview(label3)
+        
+        secondView.addSubview(label4)
+        
+        secondView.subviews.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        // TODO: 오토레이아웃 전면 수정
         NSLayoutConstraint.activate([
             secondView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -241),
             secondView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
@@ -207,22 +145,28 @@ class SecondView : UIView {
             purchaseBtn.leadingAnchor.constraint(equalTo: secondView.leadingAnchor, constant: 46)
             
         ])
+        
     }
     
-    func configuration(){
+    func configureView() {
+        self.layer.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+        secondView.addStyles(style: secondViewStyle)
+        purchaseBtn.addStyles(style: purchaseBtnStyle)
+        imageView.addStyles(style: imageViewStyle)
+        gemImageView.addStyles(style: gemImageViewStyle)
+        leftButtonImageView.addStyles(style: leftButtonImageViewStyle)
+        rightButtonImageImageView.addStyles(style: rightButtonImageViewStyle)
+        label1.addStyles(style: label1Style)
+        label2.addStyles(style: label2Style)
+        label3.addStyles(style: label3Style)
+        label4.addStyles(style: label4Style)
+    }
+    
+    func bind() {
         self.addGestureRecognizer(tapGesture)
     }
     
-    @objc func vertifyAndDismissSecondView(){
-        delegate?.confirmAndDismissSecondView()
-    }
     
-    @objc func dismissSecondView(_ sender : UITapGestureRecognizer){
-        //투명한 화면 뿐 아니라 하얀 화면을 클릭해도 함수가 발동되어 제약 사항 추가
-        if sender.view?.hitTest(sender.location(in: self), with: nil) is Self{
-            delegate?.dismissSecondView()
-        }
-    }
 }
 
 #if canImport(SwiftUI) && DEBUG
