46836c06dd580f2051286745f8d1babdbdac2880
SChan-Kim <instufld@gmail.com>
2022-10-07 15:14:12 +0900

    refactor: korKeyboardView 에서 stackView와 UI 컴포넌트 분리

---
+++ K-Keyboard/View/Keyboard/KorKeyboardView+Action.swift
@@ -0,0 +1,167 @@
+//
+//  KorKeyboardView+Action.swift
+//  K-Keyboard
+//
+//  Created by channy on 2022/10/07.
+//
+
+import UIKit
+
+extension KorKeyboardView {
+    @objc func korButtonPressed(_ sender: UIButton) {
+        print(sender.currentTitle!)
+        let char = sender.currentTitle!
+
+        if KoreanData.vowel.contains(char) {
+            switch state {
+            case 0:
+                delegate?.insertCharacter(char)
+            case 0b10000:
+                stack.append(char)
+                state |= (1 << 3)
+                delegate?.deleteCharacterBeforeCursor()
+                delegate?.insertCharacter(letter)
+            case 0b11000:
+                if KoreanData.medialVowel[stack.last! + char] != nil {
+                    stack.append(char)
+                    state |= (1 << 2)
+                    delegate?.deleteCharacterBeforeCursor()
+                    delegate?.insertCharacter(letter)
+                } else {
+                    stack.removeAll()
+                    state = 0
+                    delegate?.insertCharacter(char)
+                }
+
+            default:
+                if (state & 1) != 0 {
+                    delegate?.deleteCharacterBeforeCursor()
+                    let prev = stack.removeLast()
+                    state -= 1
+                    delegate?.insertCharacter(letter)
+                    stack.removeAll()
+                    stack.append(prev)
+                    stack.append(char)
+                    state = 0b11000
+                    delegate?.insertCharacter(letter)
+
+                } else if (state & (1 << 1)) != 0 {
+                    delegate?.deleteCharacterBeforeCursor()
+                    let prev = stack.removeLast()
+                    state ^= (1 << 1)
+                    delegate?.insertCharacter(letter)
+                    stack.removeAll()
+                    stack.append(prev)
+                    stack.append(char)
+                    state = 0b11000
+                    delegate?.insertCharacter(letter)
+
+                } else {
+                    print("모음 예외 케이스 발생")
+                }
+            }
+
+        } else {
+            switch state {
+            case 0:
+                stack.append(char)
+                state |= (1 << 4)
+                delegate?.insertCharacter(letter)
+
+            case 0b10000:
+                stack.removeAll()
+                stack.append(char)
+                delegate?.insertCharacter(letter)
+            case 0b11000, 0b11100:
+                stack.append(char)
+                state |= (1 << 1)
+                delegate?.deleteCharacterBeforeCursor()
+                delegate?.insertCharacter(letter)
+
+            case 0b11010, 0b11110:
+                if KoreanData.finalConsonant[stack.last! + char] != nil {
+                    stack.append(char)
+                    state |= (1 << 0)
+                    delegate?.deleteCharacterBeforeCursor()
+                    delegate?.insertCharacter(letter)
+
+                } else {
+                    stack.removeAll()
+                    stack.append(char)
+                    state = 0b10000
+                    delegate?.insertCharacter(letter)
+                }
+
+            default:
+                if (state & 1) != 0 {
+                    stack.removeAll()
+                    stack.append(char)
+                    state = 0b10000
+                    delegate?.insertCharacter(letter)
+
+                } else {
+                    print("자음 예외 케이스 발생")
+                }
+            }
+        }
+        
+        if self.isShiftPressed == true {
+            shiftPressed()
+        }
+    }
+
+    @objc func deletePressed() {
+        if stack.isEmpty {
+            delegate?.deleteCharacterBeforeCursor()
+        } else {
+            stack.removeLast()
+
+            var pos = 0
+            for i in 0 ..< 5 {
+                if (state & (1 << i)) != 0 {
+                    pos = i
+                    break
+                }
+            }
+
+            state ^= (1 << pos)
+            delegate?.deleteCharacterBeforeCursor()
+            delegate?.insertCharacter(letter)
+        }
+        
+        if self.isShiftPressed == true {
+            shiftPressed()
+        }
+    }
+
+    @objc func spacePressed() {
+        if stack.isEmpty {
+            delegate?.insertCharacter(" ")
+        } else {
+            stack.removeAll()
+            state = 0
+        }
+        
+        if self.isShiftPressed == true {
+            shiftPressed()
+        }
+    }
+    
+    @objc func shiftPressed() {
+        if self.isShiftPressed {
+            print("change to X")
+            self.isShiftPressed = false
+            DispatchQueue.main.async {
+                self.horizontal1.isHidden = false
+                self.horizontal1Shift.isHidden = true
+            }
+        } else {
+            print("Shift pressed")
+            self.isShiftPressed = true
+            DispatchQueue.main.async {
+                self.horizontal1.isHidden = true
+                self.horizontal1Shift.isHidden = false
+            }
+        }
+    }
+}
--- K-Keyboard/View/Keyboard/KorKeyboardView.swift
+++ K-Keyboard/View/Keyboard/KorKeyboardView.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+protocol KorKeyboardViewDelegate: class {
+    func insertCharacter(_ input: String)
+    func deleteCharacterBeforeCursor()
+}
+
 class KorKeyboardView: UIView {
 
     weak var delegate: KorKeyboardViewDelegate?
@@ -14,130 +19,34 @@ class KorKeyboardView: UIView {
     var state = 0
     var letter: String { return KoreanData.letter(stack, state) }
     var isShiftPressed: Bool = false
+    
+    lazy var horizontal1 = UIStackView()
+    lazy var horizontal1Shift = UIStackView()
+    lazy var horizontal2 = UIStackView()
+    lazy var horizontal3 = UIStackView()
 
-    static let row1 = "ㅂㅈㄷㄱㅅㅛㅕㅑㅐㅔ".map { String($0) }
-    static let row1Shift = "ㅃㅉㄸㄲㅆㅛㅕㅑㅒㅖ".map { String($0) }
-    static let row2 = "ㅁㄴㅇㄹㅎㅗㅓㅏㅣ".map { String($0) }
-    static let row3 = "ㅋㅌㅊㅍㅠㅜㅡ".map { String($0) }
-
-    let horizontal1: UIStackView = {
-        let stackView = UIStackView()
-
-        for char in row1 {
-            let button = UIButton()
-            button.layer.cornerRadius = 5
-            button.setTitle(char, for: .normal)
-            button.backgroundColor = .white
-            button.setTitleColor(.black, for: .normal)
-            button.addTarget(self, action: #selector(korButtonPressed), for: .touchUpInside)
-            stackView.addArrangedSubview(button)
-        }
-
-        stackView.axis = .horizontal
-        stackView.spacing = 6
-        stackView.distribution = .fillEqually
-        stackView.isLayoutMarginsRelativeArrangement = true
-        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
-
-        return stackView
+    let shift: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "shift"), for: .normal)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .white
+        button.tintColor = .black
+        button.contentMode = .scaleAspectFit
+        button.addTarget(self, action: #selector(shiftPressed), for: .touchUpInside)
+        
+        return button
     }()
     
-    let horizontal1Shift: UIStackView = {
-        let stackView = UIStackView()
+    let delete: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "delete.backward"), for: .normal)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .white
+        button.tintColor = .black
+        button.contentMode = .scaleAspectFit
+        button.addTarget(self, action: #selector(deletePressed), for: .touchUpInside)
 
-        for char in row1Shift {
-            let button = UIButton()
-            button.layer.cornerRadius = 5
-            button.setTitle(char, for: .normal)
-            button.backgroundColor = .white
-            button.setTitleColor(.black, for: .normal)
-            button.addTarget(self, action: #selector(korButtonPressed), for: .touchUpInside)
-            stackView.addArrangedSubview(button)
-        }
-
-        stackView.axis = .horizontal
-        stackView.spacing = 6
-        stackView.distribution = .fillEqually
-        stackView.isLayoutMarginsRelativeArrangement = true
-        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
-        stackView.isHidden = true
-
-        return stackView
-    }()
-
-    let horizontal2: UIStackView = {
-        let stackView = UIStackView()
-
-        for char in row2 {
-            let button = UIButton()
-            button.layer.cornerRadius = 5
-            button.setTitle(char, for: .normal)
-            button.backgroundColor = .white
-            button.setTitleColor(.black, for: .normal)
-            button.addTarget(self, action: #selector(korButtonPressed), for: .touchUpInside)
-            stackView.addArrangedSubview(button)
-        }
-
-        stackView.axis = .horizontal
-        stackView.spacing = 6
-        stackView.distribution = .fillEqually
-        stackView.isLayoutMarginsRelativeArrangement = true
-        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-
-        return stackView
-    }()
-
-    let horizontal3: UIStackView = {
-        let stackView = UIStackView()
-
-        let shift: UIButton = {
-            let button = UIButton()
-            button.setBackgroundImage(UIImage(systemName: "shift"), for: .normal)
-            button.layer.cornerRadius = 5
-            button.backgroundColor = .white
-            button.tintColor = .black
-            button.contentMode = .scaleAspectFit
-            button.addTarget(self, action: #selector(shiftPressed), for: .touchUpInside)
-            
-            return button
-        }()
-
-        let delete: UIButton = {
-            let button = UIButton()
-            button.setBackgroundImage(UIImage(systemName: "delete.backward"), for: .normal)
-            button.layer.cornerRadius = 5
-            button.backgroundColor = .white
-            button.tintColor = .black
-            button.contentMode = .scaleAspectFit
-            button.addTarget(self, action: #selector(deletePressed), for: .touchUpInside)
-
-            return button
-        }()
-
-        stackView.addArrangedSubview(shift)
-
-        for char in row3 {
-            let button = UIButton()
-            button.layer.cornerRadius = 5
-            button.setTitle(char, for: .normal)
-            button.backgroundColor = .white
-            button.setTitleColor(.black, for: .normal)
-            button.addTarget(self, action: #selector(korButtonPressed), for: .touchUpInside)
-            stackView.addArrangedSubview(button)
-        }
-
-        stackView.addArrangedSubview(delete)
-
-        stackView.axis = .horizontal
-        stackView.spacing = 6
-        stackView.distribution = .fillEqually
-        stackView.isLayoutMarginsRelativeArrangement = true
-        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
-
-        stackView.setCustomSpacing(16, after: shift)
-        stackView.setCustomSpacing(16, after: stackView.arrangedSubviews[row3.count])
-
-        return stackView
+        return button
     }()
 
     let spaceBar: UIButton = {
@@ -162,12 +71,119 @@ class KorKeyboardView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
 
+        horizontal1 = makeHorizontal1()
+        horizontal1Shift = makeHorizontal1Shift()
+        horizontal2 = makeHorizontal2()
+        horizontal3 = makeHorizontal3(shift: self.shift, delete: self.delete)
+        
         addViews()
         setConstraints()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+}
+
+extension KorKeyboardView {
+    
+    func makeHorizontal1() -> UIStackView {
+        let row1 = "ㅂㅈㄷㄱㅅㅛㅕㅑㅐㅔ".map { String($0) }
+        let stackView = UIStackView()
+
+        for char in row1 {
+            let button = UIButton()
+            button.layer.cornerRadius = 5
+            button.setTitle(char, for: .normal)
+            button.backgroundColor = .white
+            button.setTitleColor(.black, for: .normal)
+            button.addTarget(self, action: #selector(korButtonPressed), for: .touchUpInside)
+            stackView.addArrangedSubview(button)
+        }
+
+        stackView.axis = .horizontal
+        stackView.spacing = 6
+        stackView.distribution = .fillEqually
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+
+        return stackView
+    }
+    
+    func makeHorizontal2() -> UIStackView {
+        let row2 = "ㅁㄴㅇㄹㅎㅗㅓㅏㅣ".map { String($0) }
+        let stackView = UIStackView()
+
+        for char in row2 {
+            let button = UIButton()
+            button.layer.cornerRadius = 5
+            button.setTitle(char, for: .normal)
+            button.backgroundColor = .white
+            button.setTitleColor(.black, for: .normal)
+            button.addTarget(self, action: #selector(korButtonPressed), for: .touchUpInside)
+            stackView.addArrangedSubview(button)
+        }
+
+        stackView.axis = .horizontal
+        stackView.spacing = 6
+        stackView.distribution = .fillEqually
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+
+        return stackView
+    }
+    
+    func makeHorizontal1Shift() -> UIStackView {
+        let row1Shift = "ㅃㅉㄸㄲㅆㅛㅕㅑㅒㅖ".map { String($0) }
+        let stackView = UIStackView()
+
+        for char in row1Shift {
+            let button = UIButton()
+            button.layer.cornerRadius = 5
+            button.setTitle(char, for: .normal)
+            button.backgroundColor = .white
+            button.setTitleColor(.black, for: .normal)
+            button.addTarget(self, action: #selector(korButtonPressed), for: .touchUpInside)
+            stackView.addArrangedSubview(button)
+        }
+
+        stackView.axis = .horizontal
+        stackView.spacing = 6
+        stackView.distribution = .fillEqually
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+        stackView.isHidden = true
+
+        return stackView
+    }
+    
+    func makeHorizontal3(shift: UIButton, delete: UIButton) -> UIStackView {
+        let row3 = "ㅋㅌㅊㅍㅠㅜㅡ".map { String($0) }
+        let stackView = UIStackView()
+        stackView.addArrangedSubview(shift)
+
+        for char in row3 {
+            let button = UIButton()
+            button.layer.cornerRadius = 5
+            button.setTitle(char, for: .normal)
+            button.backgroundColor = .white
+            button.setTitleColor(.black, for: .normal)
+            button.addTarget(self, action: #selector(korButtonPressed), for: .touchUpInside)
+            stackView.addArrangedSubview(button)
+        }
+
+        stackView.addArrangedSubview(delete)
+
+        stackView.axis = .horizontal
+        stackView.spacing = 6
+        stackView.distribution = .fillEqually
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
+
+        stackView.setCustomSpacing(16, after: shift)
+        stackView.setCustomSpacing(16, after: stackView.arrangedSubviews[row3.count])
+
+        return stackView
     }
 
 }
@@ -202,164 +218,5 @@ extension KorKeyboardView {
             returnButton.bottomAnchor.constraint(equalTo: spaceBar.bottomAnchor),
             returnButton.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3)
         ])
-    }
-}
-
-extension KorKeyboardView {
-    @objc func korButtonPressed(_ sender: UIButton) {
-        print(sender.currentTitle!)
-        let char = sender.currentTitle!
-
-        if KoreanData.vowel.contains(char) {
-            switch state {
-            case 0:
-                delegate?.insertCharacter(char)
-            case 0b10000:
-                stack.append(char)
-                state |= (1 << 3)
-                delegate?.deleteCharacterBeforeCursor()
-                delegate?.insertCharacter(letter)
-            case 0b11000:
-                if KoreanData.medialVowel[stack.last! + char] != nil {
-                    stack.append(char)
-                    state |= (1 << 2)
-                    delegate?.deleteCharacterBeforeCursor()
-                    delegate?.insertCharacter(letter)
-                } else {
-                    stack.removeAll()
-                    state = 0
-                    delegate?.insertCharacter(char)
-                }
-
-            default:
-                if (state & 1) != 0 {
-                    delegate?.deleteCharacterBeforeCursor()
-                    let prev = stack.removeLast()
-                    state -= 1
-                    delegate?.insertCharacter(letter)
-                    stack.removeAll()
-                    stack.append(prev)
-                    stack.append(char)
-                    state = 0b11000
-                    delegate?.insertCharacter(letter)
-
-                } else if (state & (1 << 1)) != 0 {
-                    delegate?.deleteCharacterBeforeCursor()
-                    let prev = stack.removeLast()
-                    state ^= (1 << 1)
-                    delegate?.insertCharacter(letter)
-                    stack.removeAll()
-                    stack.append(prev)
-                    stack.append(char)
-                    state = 0b11000
-                    delegate?.insertCharacter(letter)
-
-                } else {
-                    print("모음 예외 케이스 발생")
-                }
-            }
-
-        } else {
-            switch state {
-            case 0:
-                stack.append(char)
-                state |= (1 << 4)
-                delegate?.insertCharacter(letter)
-
-            case 0b10000:
-                stack.removeAll()
-                stack.append(char)
-                delegate?.insertCharacter(letter)
-            case 0b11000, 0b11100:
-                stack.append(char)
-                state |= (1 << 1)
-                delegate?.deleteCharacterBeforeCursor()
-                delegate?.insertCharacter(letter)
-
-            case 0b11010, 0b11110:
-                if KoreanData.finalConsonant[stack.last! + char] != nil {
-                    stack.append(char)
-                    state |= (1 << 0)
-                    delegate?.deleteCharacterBeforeCursor()
-                    delegate?.insertCharacter(letter)
-
-                } else {
-                    stack.removeAll()
-                    stack.append(char)
-                    state = 0b10000
-                    delegate?.insertCharacter(letter)
-                }
-
-            default:
-                if (state & 1) != 0 {
-                    stack.removeAll()
-                    stack.append(char)
-                    state = 0b10000
-                    delegate?.insertCharacter(letter)
-
-                } else {
-                    print("자음 예외 케이스 발생")
-                }
-            }
-        }
-        
-        if self.isShiftPressed == true {
-            shiftPressed()
-        }
-    }
-
-    @objc func deletePressed() {
-        if stack.isEmpty {
-            delegate?.deleteCharacterBeforeCursor()
-        } else {
-            stack.removeLast()
-
-            var pos = 0
-            for i in 0 ..< 5 {
-                if (state & (1 << i)) != 0 {
-                    pos = i
-                    break
-                }
-            }
-
-            state ^= (1 << pos)
-            delegate?.deleteCharacterBeforeCursor()
-            delegate?.insertCharacter(letter)
-        }
-        
-        if self.isShiftPressed == true {
-            shiftPressed()
-        }
-    }
-
-    @objc func spacePressed() {
-        if stack.isEmpty {
-            delegate?.insertCharacter(" ")
-        } else {
-            stack.removeAll()
-            state = 0
-        }
-        
-        if self.isShiftPressed == true {
-            shiftPressed()
-        }
-    }
-    
-    @objc func shiftPressed() {
-        if self.isShiftPressed {
-            print("change to X")
-            self.isShiftPressed = false
-            DispatchQueue.main.async {
-                self.horizontal1.isHidden = false
-                self.horizontal1Shift.isHidden = true
-            }
-        } else {
-            print("Shift pressed")
-            self.isShiftPressed = true
-            DispatchQueue.main.async {
-                self.horizontal1.isHidden = true
-                self.horizontal1Shift.isHidden = false
-            }
-        }
     }
 }
