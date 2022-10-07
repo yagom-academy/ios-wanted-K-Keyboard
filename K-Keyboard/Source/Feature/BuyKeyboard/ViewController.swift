bcc89f35cc1e2740ac0948e2483fdd7dc02e05b9
강민교 <98341623+KangMinGyo@users.noreply.github.com>
2022-10-07 13:16:31 +0900

    feat: Comment 등록 기능 추가

--- K-Keyboard/Source/Feature/BuyKeyboard/ViewController.swift
+++ K-Keyboard/Source/Feature/BuyKeyboard/ViewController.swift
@@ -8,11 +8,172 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let buyView: BuyView = {
+       let view = BuyView()
+        return view
+    }()
+    
+    var toolbarState: Bool?
+    var maxLength = 20 //글자수
+    
+    let commentTableView = UITableView()
+    var comment = [String]()
+    var height = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+        view.backgroundColor = .systemBackground
+        buyView.viewController = self
+        
+        buyView.buyButton.addTarget(self, action: #selector(buyButtonPressed), for: .touchUpInside)
+        
+        hideKeyboardWhenTappedAround() //키보드내리기
+        enableKeyboardHideOnTap()
+        stringNumber()//글자수제한
+        
+        toolbarState = true
+        
+        //테이블뷰
+        configure()
+        setupTableView()
+        commentTableView.register(CommentTableViewCell.self, forCellReuseIdentifier: "cell")
+        commentTableView.isScrollEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("호출완료")
+        setUPReturnForeground()
+    }
+    
+    func configure() {
+        buyView.contentView.addSubview(commentTableView)
+        commentTableView.translatesAutoresizingMaskIntoConstraints = false
+        commentTableView.topAnchor.constraint(equalTo: buyView.timeLabel.bottomAnchor).isActive = true
+        commentTableView.bottomAnchor.constraint(equalTo: buyView.contentView.bottomAnchor, constant: -50).isActive = true
+        commentTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        commentTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        commentTableView.heightAnchor.constraint(equalToConstant: CGFloat(height)).isActive = true
+        commentTableView.rowHeight = 120
+        commentTableView.separatorStyle = .none
+    }
+    
+    func setupTableView() {
+        commentTableView.delegate = self
+        commentTableView.dataSource = self
+    }
+    
+    private func stringNumber() {
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange), name: UITextField.textDidChangeNotification, object: nil)
+    }
+    
+    @objc private func textDidChange(_ notification: Notification) {
+            if let textField = notification.object as? UITextField {
+                if let text = textField.text {
+                    
+                    if text.count > maxLength {
+                        //20글자 넘어가면 자동으로 키보드 내려가도록
+                        textField.resignFirstResponder()
+                    }
+                    
+                    //초과되는 텍스트 제거
+                    if text.count >= maxLength {
+                        let index = text.index(text.startIndex, offsetBy: maxLength)
+                        let newString = text[text.startIndex..<index]
+                        buyView.commentTextField.text = String(newString)
+                    }
+                }
+            }
+        }
+    
+    private func enableKeyboardHideOnTap() {
+        NotificationCenter.default.addObserver(self, selector: #selector(toolbarMoveUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(toolbarMoveDown), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func toolbarMoveUp(_ notification: NSNotification){
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+          let keybaordRectangle = keyboardFrame.cgRectValue
+          let keyboardHeight = keybaordRectangle.height
+          buyView.commentToolbar.frame.origin.y -= keyboardHeight
+        }
+    }
+        
+    @objc func toolbarMoveDown(_ notification: NSNotification){
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keybaordRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keybaordRectangle.height
+            buyView.commentToolbar.frame.origin.y += keyboardHeight
+        }
+    }
+    
+    func setUPReturnForeground() {
+        if toolbarState == true {
+            buyView.toolbar.isHidden = false
+        } else {
+            buyView.toolbar.isHidden = true
+        }
+    }
+    
+    @objc func buyButtonPressed() {
+        let nextVC = PopUpViewController()
+        nextVC.modalPresentationStyle = .overCurrentContext
+        toolbarState = false
+        present(nextVC, animated: false)
+    }
+    
+    @objc func commentButtonPressed() {
+        let value = buyView.commentTextField.text ?? ""
+        if value == "" {
+            let alert = UIAlertController(title: "댓글을 입력해주세요", message: "확인을 눌러주세요", preferredStyle: UIAlertController.Style.alert)
+            let cancel = UIAlertAction(title: "확인", style: .default, handler: nil)
+            alert.addAction(cancel)
+            present(alert, animated: true, completion: nil)
+        } else {
+            commentTableView.removeConstraints(commentTableView.constraints)
+            comment.append(value)
+            height += 120
+            configure()
+            print(height)
+        }
+        buyView.reviewLabel.text = "구매 리뷰 \(comment.count)"
+        buyView.commentTextField.text = ""
+        buyView.commentTextField.placeholder = "댓글을 남겨보세요!"
+        commentTableView.reloadData() //이거 이렇게 해도 되는건가?
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return comment.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CommentTableViewCell
+        cell.selectionStyle = .none
+        cell.profileImageView.image = UIImage(named: "Profile")
+        cell.nicknameLabel.text = "닉네임"
+        cell.commentLabel.text = comment[indexPath.row]
+        cell.timeLabel.text = "1초"
+        return cell
+    }
+    
 
 }
 
+// 키보드 숨기기
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
